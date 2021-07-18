b21c65801d8161641aa7a985d6133ed9cbada9c4
Dandi-Raihan_Harianto <dandyrayhan1@gmail.com>
2021-07-18 20:21:42 +0700

    quiz1

---
+++ resources/views/track/index.blade.php
@@ -0,0 +1,38 @@
+
+@extends('layouts.app') 
+
+@section('content') 
+<div class="container">
+
+    <h3>PLAYED</h3>
+    <h4>
+    <a href="{{ url('/track/create') }}"class="btn btn-primary btn-sm float-right">MASUKKAN DATA</a>
+    </h4>
+    <table class="table table-bordered" >
+        <tr>
+            <th>Track ID</th>
+            <th>Track Nama</th>
+            <th>Artist ID</th>
+            <th>Album ID</th>
+            <th>DELETE</th>
+
+        </tr>
+            @foreach($rows as $row)
+        <tr>
+            <td>{{ $row->track_id }}</td>
+            <td>{{ $row->track_nama }}</td>
+            <td>{{ $row->artist_id }}</td>
+            <td>{{ $row->album_id }}</td>
+
+    <td>
+    <form action="{{ url('/track/' . $row->track) }}" method="POST">
+            <input name="_method" type="hidden" value="DELETE">
+            @csrf
+        <button class="btn btn-danger btn-sm">Hapus</button>
+    </form>
+    </td>
+        </tr>
+            @endforeach
+    </table>
+ </div> 
+@endsection 
